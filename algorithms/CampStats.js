7c9063ceec03f47e347582858c0aec6c36d53887
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 <YuriSobioch@gmail.com>
2023-03-26 04:39:47 -0600

    directional function added

--- algorithms/CampStats.js
+++ algorithms/CampStats.js
@@ -1,24 +1,52 @@
 class Camp {
-    constructor(x, y, jg) {
+    constructor(name, x, y, jg) {
+        this.name = name;
         this.x = x;
         this.y = y;
         this.jg = jg;
     }
 }
 
-let bBramble = new Camp(7500, 3200, bRed);
-let bKrugs = new Camp(8200, 2700, bRed);
-let bRaptors = new Camp(7000, 4800, bRed);
-let bWolves = new Camp(3200, 6700, bBlue);
-let bSentinal = new Camp(3200, 8150, bBlue);
-let bGromp = new Camp(2400, 8350, bBlue);
+let bRed = 'Blue side, red Jungle';
+let bBlue = 'Blue side, blue Jungle';
+let rRed = 'Red side, red Jungle';
+let rBlue = 'Red side, blue Jungle';
 
-let rSentinal = new Camp(11400, 6900, rBlue);
-let rGromp = new Camp(12300, 6700, rBlue);
-let rWolves = new Camp(11300, 8300, rBlue);
-let rRaptors = new Camp(8000, 10200, rRed);
-let rBramble = new Camp(7500, 11800, rRed);
-let rKrugs = new Camp(6800, 12300, rRed);
+let bBramble = new Camp('Bramble', 7500, 3200, bRed);
+let bKrugs = new Camp('Krugs', 8200, 2700, bRed);
+let bRaptors = new Camp('Raptors', 7000, 4800, bRed);
+let bWolves = new Camp('Wolves', 3200, 6700, bBlue);
+let bSentinal = new Camp('Sentinal', 3200, 8150, bBlue);
+let bGromp = new Camp('Gromp', 2400, 8350, bBlue);
 
-let tScuttle = new Camp(5000, 9500, tRriver);
-let bScuttle = new Camp(10000, 55000, bRiver);
+let rSentinal = new Camp('Sentinal', 11400, 6900, rBlue);
+let rGromp = new Camp('Gromp', 12300, 6700, rBlue);
+let rWolves = new Camp('Wolves', 11300, 8300, rBlue);
+let rRaptors = new Camp('Raptors', 8000, 10200, rRed);
+let rBramble = new Camp('Bramble', 7500, 11800, rRed);
+let rKrugs = new Camp('Krugs', 6800, 12300, rRed);
+
+let tScuttle = new Camp('Scuttle', 5000, 9500, 'tRriver');
+let bScuttle = new Camp('Scuttle', 10000, 55000, 'bRiver');
+
+let allCamps = [bBramble, bKrugs, bRaptors, bWolves, bSentinal, bGromp, rSentinal, rGromp, rWolves, rRaptors, rBramble, rKrugs, tScuttle, bScuttle];
+
+let closestCamp = (x, y) => {
+    let closestCamp = null;
+    let closestCordinates = [Infinity, Infinity];
+    for(const camp of allCamps) {
+        if(Math.abs((camp.x - x)) + Math.abs((camp.y - y)) < Math.abs(closestCordinates[0] - x) + Math.abs(closestCordinates[1] - y)) {
+            closestCamp = camp;
+            closestCordinates = [camp.x, camp.y]
+        }
+    }
+
+    return closestCamp;
+}
+
+let direction = (firstP, secondP) => {
+    return [closestCamp(firstP[0], firstP[1]).jg, closestCamp(secondP[0], secondP[1]).jg]
+}
+
+console.log(closestCamp(3159, 8157))
+console.log(direction([3159, 8157], [7000, 4682]))
